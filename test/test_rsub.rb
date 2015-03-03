a017f1aa1f3db8f42832e11182e42398cddbce38
Gabor Bata <bata.gabor@gmail.com>
2015-03-03 14:06:22 +0100

    Changed test data to include line breaks as well.

--- test/test_rsub.rb
+++ test/test_rsub.rb
@@ -4,7 +4,7 @@ require_relative '../bin/rsub.rb'
 class TestRsub < Test::Unit::TestCase
 
   def setup
-    @entry = SrtEntry.new(1, '00:02:17,440', '00:02:20,375', "Senator, we're making our final approach into Coruscant.")
+    @entry = SrtEntry.new(1, '00:02:17,440', '00:02:20,375', "Senator, we're making\nour final approach into <i>Coruscant</i>.")
   end
 
   def teardown
@@ -20,17 +20,17 @@ class TestRsub < Test::Unit::TestCase
   end
 
   def test_srt_entry
-    assert_equal("1\n00:02:17,439 --> 00:02:20,375\nSenator, we're making our final approach into Coruscant.\n\n", @entry.to_s)
+    assert_equal("1\n00:02:17,439 --> 00:02:20,375\nSenator, we're making\nour final approach into Coruscant.\n\n", @entry.to_s)
   end
 
   def test_srt_entry_fps
     SrtChangeCommand.new(:fps, '25').execute(@entry)
-    assert_equal("1\n00:02:11,810 --> 00:02:14,625\nSenator, we're making our final approach into Coruscant.\n\n", @entry.to_s)
+    assert_equal("1\n00:02:11,810 --> 00:02:14,625\nSenator, we're making\nour final approach into Coruscant.\n\n", @entry.to_s)
   end
 
   def test_srt_entry_shift
     SrtChangeCommand.new(:shift, 10.5).execute(@entry)
-    assert_equal("1\n00:02:27,939 --> 00:02:30,875\nSenator, we're making our final approach into Coruscant.\n\n", @entry.to_s)
+    assert_equal("1\n00:02:27,939 --> 00:02:30,875\nSenator, we're making\nour final approach into Coruscant.\n\n", @entry.to_s)
   end
 
 end
